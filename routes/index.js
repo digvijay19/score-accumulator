af766de86c73f57e25190ff3ffa6699e86efe8cc
Digvijay <digvijag@thoughtworks.com>
2013-11-29 13:26:24 +0530

    basic functionality working

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,21 @@
-
-/*
- * GET home page.
- */
-
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+var data = {
+	"1":{
+		"name":"Suraj",
+		"score":90
+	},
+	"2":{
+		"name":"Digvijay",
+		"score":90
+	}
+};
+exports.home = function(req, res){
+	res.render('home', { title: 'score-accumulator',data:data });
+};
+exports.add = function(req, res){
+	res.render('add', { title: 'Add new record'});
+};
+exports.addNewRecord = function(req, res){
+	var recordNumber = data.length+1;
+	data[recordNumber] = req.body;
+	res.redirect('/home');
 };
